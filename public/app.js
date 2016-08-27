f9c7c979edd095fd88be16606815d7d3e3f663f0
T. GREEN <toddgreen@utexas.edu>
2016-08-27 02:28:21 -0500

    160825_updates for css and modal work -- modal not displaying

--- public/app.js
+++ public/app.js
@@ -1,38 +1,42 @@
 // Get Articles as a JSON
 $.getJSON('/articles', function(data) {
   for (var i=0; i<data.length; i++) {
-    $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
+    $('#articles').append('<div class="card horizontal"> <div class="card-content"> <p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '<a class="modal-trigger" href="#Modal1"></a> </p> </div> </div>');
   }
 });
 
 // On-Click for p-Tags
-$(document).on('click', 'p', function() {
-  $('#messages').empty();
+$(function() {
+  $(document).on('click', 'p', function() {
+    $('#messages').empty();
+    $(this).openModal();
 
-  var thisID = $(this).attr('data-id');
+    var thisID = $(this).attr('data-id');
 
-  $.ajax({
-    method: 'GET',
-    url: '/articles/' + thisID,
-  })
+    $.ajax({
+      method: 'GET',
+      url: '/articles/' + thisID,
+    })
 
-    .done(function( data ) {
-      console.log(data);
-      $('#messages').append('<h2>' + data.title + '</h2>');
+      .done(function( data ) {
+        console.log(data);
+        $('#messages').append('<div id="Modal1" class="modal"> <div class="modal-content"> <h3>' + data.title + '</h3> </div> </div>');
 
-      $('#messages').append('<input id="titleinput" name="title" >');
+        $('#messages').append('<div id="Modal1" class="modal"> <div class="modal-content"> <input id="titleinput" name="title"> </div> </div>');
 
-      $('#messages').append('<textarea id="bodyinput" name="body"></textarea>');
+        $('#messages').append('<div id="Modal1" class="modal"> <div class="modal-content"> <textarea id="bodyinput" name="body"></textarea> </div> </div>');
 
-      $('#messages').append('<button data-id="' + data._id + '" id="savemessage">Save Message</button>');
+        $('#messages').append('<div id="Modal1" class="modal modal-footer"> <a href="#!" class="modal-action modal-close" data-id="' + data._id + '" id="savemessage">Save Message</a> </div>');
 
-      if(data.message) {
-        $('#titleinput').val(data.message.title);
+        if(data.message) {
+          $('#titleinput').val(data.message.title);
 
-        $('#bodyinput').val(data.message.body);
-      }
-    });
+          $('#bodyinput').val(data.message.body);
+        }
+      });
+  });
 });
+
 
 $(document).on('click', '#savemessage', function(){
   var thisID = $(this).attr('data-id');
